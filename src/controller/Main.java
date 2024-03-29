3bfd57db34ccd6473c031e2a6e57f0f1a8c6a200
Maria Andreeva <maria.andreeva.marketing@gmail.com>
2024-03-29 13:45:22 +0100

    view added

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,77 +1,114 @@
-package controller;
+package controller;/*
+Проанализировать персонажей "Крестьянин, Разбойник, Снайпер, Колдун, Копейщик, Арбалетчик, Монах".
+Для каждого определит 8 полей данных(здоровье, сила итд) 3-4 поля поведения(методов атаковать, вылечить итд).
+Создать абстрактный класс и иерархию наследников.
+Расположить классы в пакет так, чтобы в основной программе небыло видно их полей.
+В не абстрактных классах переопределить метод toString() так чтобы он возвращал название класса или имя.
+Создать в основной программе по одному обьекту каждого не абстрактного класса и вывести в консоль его имя.
+
+Добавить в абстрактный класс int поле инициатива. В классах наследников инициализировать это поле.
+Крестьянин = 0, маги=1, пехота=2, лучники=3.
+В мэйне сделать так, чтобы сначала делали ход персонажи с наивысшей инициативой
+из обеих комманд а с наименьшей в конце.
+*/
 
 import behavior.CoordXY;
 import behavior.HeroesNames;
 import person.*;
+import view.View;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
-    static ArrayList<PersonBase> red = new ArrayList<>();
-    static ArrayList<PersonBase> blue = new ArrayList<>();
-    static ArrayList<PersonBase> all = new ArrayList<>();
+    public static ArrayList<PersonBase> greenPersons = new ArrayList<>();
+    public static ArrayList<PersonBase> bluePersons = new ArrayList<>();
+    public static ArrayList<PersonBase> allPersons = new ArrayList<>();
 
     public static void main(String[] args) {
-        createTeam(red, 10, 0);
-        createTeam(blue, 10, 3);
-        all.addAll(red);
-        all.addAll(blue);
+        createTeam(greenPersons, 10, 0);
+        createTeam(bluePersons, 10, 3);
+        allPersons.addAll(bluePersons);
+        allPersons.addAll(greenPersons);
 //        all.sort(new PrioritySort());
-        all.sort((o1, o2) -> Integer.compare(o2.priority, o1.priority));
+        allPersons.sort((o1, o2) -> Integer.compare(o2.priority, o1.priority));
 
-        for (int i = 0; i <=10; i ++ ){
-
-
-            for (PersonBase p : all) {
-                System.out.print(p + " ходит. ");
-                if (red.contains(p)) {
-//                if (p.getClass().getSimpleName().equals("Wizard"))
-//                {
-//                    ...
-//                }
-                    p.step(blue, red);
+//        for (int i = 0; i < 30; i++) {
+        Scanner in = new Scanner(System.in);
+        while (true)
+        {
+            View.view();
+            for (PersonBase p : allPersons) {
+//                System.out.print(p + " ходит. ");
+                if (greenPersons.contains(p)) {
+                    p.step(bluePersons, greenPersons);
 
                 } else {
-                    p.step(red, blue);
+                    p.step(greenPersons, bluePersons);
                 }
-                System.out.println();
+//                System.out.println();
             }
+            in.nextLine();
+            if (!isLiving(greenPersons))
+            {
+                System.out.println("Blue team wins!");
+                break;
+            }
+            if (!isLiving(bluePersons))
+            {
+                System.out.println("Green wins!");
+                break;
+            }
+
         }
 
     }
 
+    private static boolean isLiving(ArrayList<PersonBase> team)
+    {
+        for (PersonBase personBase : team) {
+            if (personBase.getHealth() > 0)
+                return true;
+        }
+        return false;
+    }
 
-    public static void createTeam(ArrayList<PersonBase> team, int num, int start) {
+    public static void createTeam(ArrayList<PersonBase> team, int num, int start)
+    {
         Random rnd = new Random();
-        while (--num >= 0) {
+        int cy = 0;
+        while (num-- > 0)
+        {
             int n = start + rnd.nextInt(4);
-            switch (n) {
+            switch (n)
+            {
                 case 0:
-                    team.add(new Crossbowman(HeroesNames.getRandomName(), new CoordXY(0, num)));
+                    team.add(new Crossbowman(HeroesNames.getRandomName(), new CoordXY(9, cy)));
                     break;
                 case 1:
-                    team.add(new Spearman(HeroesNames.getRandomName(), new CoordXY(0, num)));
+                    team.add(new Spearman(HeroesNames.getRandomName(), new CoordXY(9, cy)));
                     break;
                 case 2:
-                    team.add(new Wizard(HeroesNames.getRandomName(), new CoordXY(0, num)));
+                    team.add(new Wizard(HeroesNames.getRandomName(), new CoordXY(9, cy)));
                     break;
                 case 3:
-                    team.add(new Peasant(HeroesNames.getRandomName(), new CoordXY(start * 3, num)));
+                    team.add(new Peasant(HeroesNames.getRandomName(), new CoordXY((3-start)*3, cy)));
                     break;
                 case 4:
-                    team.add(new Sniper(HeroesNames.getRandomName(), new CoordXY(9, num)));
+                    team.add(new Sniper(HeroesNames.getRandomName(), new CoordXY(0, cy)));
                     break;
                 case 5:
-                    team.add(new Monk(HeroesNames.getRandomName(), new CoordXY(9, num)));
+                    team.add(new Monk(HeroesNames.getRandomName(), new CoordXY(0, cy)));
                     break;
                 case 6:
-                    team.add(new Robber(HeroesNames.getRandomName(), new CoordXY(9, num)));
+                    team.add(new Robber(HeroesNames.getRandomName(), new CoordXY(0, cy)));
                     break;
                 default:
                     System.out.println("ERROR! Пересмотри алгоритм, ламер!");
             }
+            cy++;
         }
     }
 
